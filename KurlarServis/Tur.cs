91e7065f7faf424b66571ea68a7d91f1316a4f5f
kaannc7 <kaannc7@gmail.com>
2018-01-27 14:37:38 +0300

    Revert "Revert "Döviz Kurları Çekme Servis""
    This reverts commit 69763ee9bacb9c04fc983d500976c3f2c31c5475.

---
+++ KurlarServis/Tur.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KurlarServis
+{
+    public class Tur
+    {
+        public string KurTipi { get; set; }
+        public double Tutar { get; set; }
+    }
+}
